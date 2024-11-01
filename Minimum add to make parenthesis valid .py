c915ea0df586c574de9445ea05bf5844821df740
Nikita Deo <151768107+NikitaDeo@users.noreply.github.com>
2024-11-01 21:09:34 +0530

    Add files via upload

---
+++ Minimum add to make parenthesis valid .py
@@ -0,0 +1,18 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        open_needed = 0 
+
+        close_needed = 0 
+        
+        
+        for char in s:
+            if char == '(':
+                open_needed += 1
+            elif char == ')':
+                if open_needed > 0:
+                    open_needed -= 1 
+                else:
+                    close_needed += 1 
+
+
+        return open_needed + close_needed
