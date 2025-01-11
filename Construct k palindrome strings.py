5e2ec4452206af2f43d63b2b376509127c758c99
Nikita Deo <151768107+NikitaDeo@users.noreply.github.com>
2025-01-11 22:09:25 +0530

    Create Construct k palindrome strings.py

---
+++ Construct k palindrome strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if k > len(s):
+            return False
+        from collections import Counter
+        char_count = Counter(s)
+        odd_count = sum(1 for count in char_count.values() if count % 2 != 0)
+        return odd_count <= k
