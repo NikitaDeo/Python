3889f1920acc316a92fc7d0c79493cb634ce2b13
Nikita Deo <151768107+NikitaDeo@users.noreply.github.com>
2025-01-30 10:09:31 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+choclate=input("enter your fav choclate:")
+if choclate == "dairymilk":
+    print("its my favourite")
+elif choclate == "kitkat":
+    print("chandus fav")
+elif choclate == "milkybar":
+    print("mounis fav")
+else:
+    print("lavs fav")
